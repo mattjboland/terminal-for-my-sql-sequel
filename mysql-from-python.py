ba53ce547099cc8baca678295e5f023cd02bfe13
Matt Boland <mattjamesboland@gmail.com>
2020-06-03 15:46:42 +0000

    updated Bob

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,7 +1,7 @@
 import os
 import pymysql
 
-# Get username from Cloud9 workspace
+# Get the username from the Cloud9 workspace
 # (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
@@ -10,14 +10,10 @@ connection = pymysql.connect(host='localhost',
                              user=username,
                              password='',
                              db='Chinook')
+
 try:
-    # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'Bob';")
+        connection.commit()
 finally:
-    # Close the connection, regardless of whether or not the above was
-    # successful
     connection.close()
