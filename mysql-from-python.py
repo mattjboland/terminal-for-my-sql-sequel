5ff8059104dd96c2c10962addb21bb8cf7b45521
Matt Boland <mattjamesboland@gmail.com>
2020-06-03 16:50:08 +0000

    cleared tables

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -10,10 +10,15 @@ connection = pymysql.connect(host='localhost',
                              user=username,
                              password='',
                              db='Chinook')
-
 try:
     with connection.cursor() as cursor:
-        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'Bob';")
+        list_of_names = ['fred', 'Fred']
+        # Prepare a string with same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute(
+            "DELETE FROM Friends WHERE name in ({});".format(format_strings),
+            list_of_names)
+
         connection.commit()
 finally:
     connection.close()
